1053d23ece82a054fde6327ba4b64bdb5894b5f6
red1172 <red1172@gmail.com>
2019-12-13 09:49:31 +0100

    Première Photo du projet

---
+++ src/app/services/task.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Task } from 'src/app/models/task';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+  apiUrl = 'http://localhost:3000/tasks';
+  constructor(private http: HttpClient) { }
+
+  findAll() {
+    return this.http.get<Task[]>(this.apiUrl);
+  }
+
+  delete(id) {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
+
+  persist(task) {
+    return this.http.post<Task>(this.apiUrl, task);
+  }
+
+ completed(id, completed) {
+   return this.http.patch(`${this.apiUrl}/${id}`, {completed : !completed});
+ }
+ update(task) {
+   return this.http.put(`${this.apiUrl}/${task.id}`, task);
+ }
+}
